3abe02b85b588335c53ef25c7a246a807bc77b0a
sandeep rawat <rawatsandeep1989@gmail.com>
2022-10-29 20:50:09 +0530

    first_python_code

---
+++ python_video1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3 
+
+a = 30 # a is reference and 30 is an object in memory
+b = a  # created another ref b which will piont to same location as a
+print(id(a))
+print(id(b))
+
+
+a=90
+print(id(a))
+print(id(b))
+
+
+c = 12.5
+print(c)
+
+##naming convention 
+cat_90 = 30
+print(cat_90)
+
+
+# arthimetic operation in python
+a=90
+b=c=80
+print(a+b)
+print(a//9)
+
+print(10/3)
+
+print(12%3)
+
+
+
+
+
+
